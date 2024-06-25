bace3cbce9a5ba1a184bd544d0420c612b5d4bee
Stephen-Hilton <stephen@spaceandtime.io>
2024-06-25 07:44:06 -0700

    added logging

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 python3 -m venv venv_runner
 . ./venv_runner/bin/activate
+mkdir data
 
 pip3 install --upgrade pip
 pip3 install spaceandtime --upgrade
--- src/heartbeat.py
+++ src/heartbeat.py
@@ -1,15 +1,21 @@
 import requests, json, os
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
 from spaceandtime import SpaceAndTime
+
+timestamp_format = '%Y-%m-%dT%H:%M:%S+00:00'
+
+# add sxt early, for logging
+sxt = SpaceAndTime(envfile_filepath="./src/.env")
+sxt.logger_addFileHandler(Path(f'./src/logs/heartbeat_{datetime.utcnow().strftime(timestamp_format)}.log'))
 
 # load in dotenv file 
 load_dotenv('./src/.env')
 zapier_webhook = os.getenv('ZAPIER_WEBHOOK_NOTIFY_NETWORK_STATE_CHANGE')
 sxtlabs_dml_biscuit = os.getenv('SXTLABS_DML_BISCUIT')
-timestamp_format = '%Y-%m-%dT%H:%M:%S+00:00'
 test=str(os.getenv('TEST_ONLY')).strip().lower() in ['true','yes']
+sxt.logger.info(f"config settings: \nzapier webhook: {zapier_webhook} \ndml_biscuit: {sxtlabs_dml_biscuit[:6]}...{sxtlabs_dml_biscuit[-6:]} \ntest_only: {test}\n")
 
 
 # define function to report status
@@ -23,17 +29,18 @@ def report_status(webhook_url, status, message, eta: str = None):
     if eta: payload['eta'] = "ETA: " + eta
     if test:
         print('skipping API Call!')
-        return 200, {}
+        rtn = 200, {'test': 'true'}
     else:
         resp = requests.post(webhook_url, json=payload, headers=header)    
         jsonResp = resp.json()
-        return resp.status_code, jsonResp 
-
+        rtn = resp.status_code, jsonResp 
+    sxt.logger.info(f"report_status response: {rtn}")
     
 
 
 # get last heartbeat run status:
 last_status_file = Path('./src/data/last_status.json')
+sxt.logger.info(f'Getting last status from {last_status_file}')
 if last_status_file.exists():
     with open(last_status_file, 'r') as f:
         last_status_data = json.load(f)
@@ -45,14 +52,13 @@ last_status = last_status_data['STATUS']
 utc_timestamp = datetime.strptime(last_status_data['UTC_TIMESTAMP'], timestamp_format)
 hrs_since_last_notify = (datetime.utcnow() - utc_timestamp).total_seconds()/60/60
 current_hour = datetime.utcnow().hour 
+sxt.logger.info(f'Last status data: \nlast status: {last_status} \nUTC: {utc_timestamp} \nhrs_since_last_notify: {hrs_since_last_notify} \ncurrent_hour: {current_hour}\n')
 
 
 
 # Authenticate to SXT network and run canary query
-sxt = SpaceAndTime(envfile_filepath="./src/configs/.env")
 sxt.authenticate()
-success, data = sxt.execute_query("""
-    SELECT 'OK' as STATUS
+success, data = sxt.execute_query("""    SELECT 'OK' as STATUS
     , 'Space and Time Network is UP and accessible!' as MESSAGE
     from sxtlabs.SINGULARITY """)
  
@@ -67,14 +73,18 @@ else: # all is well!
     data = data[0]
     sxt.execute_query('update SXTLABS.Singularity set LAST_HEARTBEAT = current_timestamp'
                       , biscuits=[sxtlabs_dml_biscuit])
+sxt.logger.info(f"data returned: \n{data}")
 
 
 # test to see if state and last status state are different, or once every 6hrs (on the quarter-day only)
 if data['STATUS'] != last_status or (hrs_since_last_notify > 5.95 and current_hour in [0,6,12,18]) :
     report_status(zapier_webhook, data['STATUS'], data['MESSAGE'])
+else: 
+    sxt.logger.info('No status change, skipping report')
 
 
 # add current UTC time, and write new last_status to file
+sxt.logger.info('Writing new last status to file')
 data['UTC_TIMESTAMP'] = datetime.utcnow().strftime(timestamp_format)
 with open(last_status_file, 'w') as f:
     json.dump(data, f)
